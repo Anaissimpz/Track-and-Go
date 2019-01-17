8a183cafe3cc7e1909197b29683021a5fa827af5
Anaissimpz <anaissimpenzwe@gmail.com>
2019-01-17 10:59:31 +0200

    add features

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+
+function passenger(username,from,to,time,book){
+    this.username;
+    this.from;
+    this.to;
+    this.time;
+    this.book;
+}
+passenger.prototype.
+
+/*var txt = "";
+var person = {}; 
+var x;
+for (x in person) {
+  txt += person[x] + " ";
+}
+document.getElementById("container").innerHTML = txt;*/
